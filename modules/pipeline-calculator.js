91cfa71cd119d85a6ef4679866721b8cd2adc778
cbajic <89222999+cbajic@users.noreply.github.com>
2021-10-07 14:31:35 -0600

    calculator updates

--- modules/pipeline-calculator.js
+++ modules/pipeline-calculator.js
@@ -175,10 +175,10 @@ export function injectionProfile(injectionFluid, pipeline, elevationProfile) {
 
 		 console.log("velocity: " + velocity);
 
-		// if(velocity < 0.00001){
-		// 	alert("Pig speed low: " + (velocity * 3.6 * 1000).toFixed(2) + " meters/hour at: " + (previousBackofSlug).toFixed(2));
-		// 	return outputArrayOfObjects;
-		// }
+		if(velocity < 0.0001){
+			alert("Pig speed low: " + (velocity * 3.6 * 1000).toFixed(2) + " meters/hour at: " + (previousBackofSlug).toFixed(2));
+			return outputArrayOfObjects;
+		}
 		
 
 		if(backOfSlug < 10){
